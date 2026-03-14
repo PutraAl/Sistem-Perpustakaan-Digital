fa617440b5ca5817e6699bb6708b770c78e9331c
mohammadnurferli25-boop <mohammadnurferli25@gmail.com>
2026-03-15 06:10:31 +0700

    Update BukuController menambahkan function index

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class BukuController extends Controller
 {
-    //
+    public function index()
+    {
+        $buku = [
+            ["id"=>1,"nama"=>"Pemrograman Web"],
+            ["id"=>2,"nama"=>"Basis Data"],
+            ["id"=>3,"nama"=>"Algoritma"]
+        ];
+
+        return view('buku.index', compact('buku'));
+    }
 }
+ 
