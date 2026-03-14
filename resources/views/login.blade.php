5e5d0acedcda9d88cc92098d80cf9a20661c87dc
Putra Al <palamsyah55@gmail.com>
2026-03-15 02:24:16 +0700

    Menambahkan 3 routing sementara serta membuat view dan controller Login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Selamat datang di halaman login sistem perpustakaan
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\BukuController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/buku', [BukuController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
