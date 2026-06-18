7efa382f9d7eeab2b50904f6def0117bcf159ed0
Putra Al <palamsyah55@gmail.com>
2026-06-18 19:57:25 +0700

    feat : routing

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -7,7 +7,7 @@
     class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
 
     <div class="relative p-4 w-full max-w-md max-h-full">
-        <div class="relative bg-sky-blue-600 border border-default rounded-base shadow-sm p-4 md:p-6">
+        <div class="relative bg-sky-200 border border-default rounded-md shadow-sm p-4 md:p-6">
 
             <!-- Header -->
             <div class="flex items-center justify-between border-b border-default pb-4 md:pb-5">
